a6619598691564caf2b8c922cc7b213516332e40
nestor2208 <nestoralandete2208@gmail.com>
2026-06-04 16:30:28 -0500

    Se agregaron comentarios que dan como ejemplo la calificacion de la receta entregada, y el error de validacion si dicha calificacion esta fuera del limite establecido

--- tests/test_calificacion.py
+++ tests/test_calificacion.py
@@ -2,10 +2,12 @@ import pytest
 from pydantic import ValidationError
 from app.schemas.schemas import CalificacionCreate
 
+#Calificaciones
 def test_calificacion_valida():
     cal = CalificacionCreate(estrellas=5)
     assert cal.estrellas == 5
 
+#Error en caso de que la calificacion contenga un valor fuera de los establecidos
 def test_calificacion_fuera_de_rango_alto():
     with pytest.raises(ValidationError):
         CalificacionCreate(estrellas=6)
