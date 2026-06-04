a3ac20fcaee37083639c616d0dcfffcaadda8ea2
rafalegartor05 <rafalegartor05@gmail.com>
2026-06-03 20:56:44 -0500

    Se agrego un comentario en el proceso de calificacion de las recetas

--- tests/test_calificacion.py
+++ tests/test_calificacion.py
@@ -2,6 +2,8 @@ import pytest
 from pydantic import ValidationError
 from app.schemas.schemas import CalificacionCreate
 
+#Archivo Proceso Calificacion de Estrellas
+
 def test_calificacion_valida():
     cal = CalificacionCreate(estrellas=5)
     assert cal.estrellas == 5
@@ -13,3 +15,4 @@ def test_calificacion_fuera_de_rango_alto():
 def test_calificacion_fuera_de_rango_bajo():
     with pytest.raises(ValidationError):
         CalificacionCreate(estrellas=0)
+
